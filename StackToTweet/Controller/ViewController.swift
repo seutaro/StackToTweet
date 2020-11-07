51a528499f4636a2ec5eea6cf76077eef69f8e0a
seutaro <seutaros@gmail.com>
2020-11-08 01:48:57 +0900

    ReusableTableViewController.swift作成

--- StackToTweet/Controller/ViewController.swift
+++ StackToTweet/Controller/ViewController.swift
@@ -9,7 +9,31 @@ import UIKit
 import Parchment
 import RealmSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PagingViewControllerDataSource {
+    
+    let realm = try! Realm()
+    var categories:Category? {
+        didSet {
+            //ここにItemをロードする関数
+        }
+    }
+    
+    
+    
+    
+    
+    func numberOfViewControllers(in pagingViewController: PagingViewController) -> Int {
+        <#code#>
+    }
+    
+    func pagingViewController(_: PagingViewController, viewControllerAt index: Int) -> UIViewController {
+        <#code#>
+    }
+    
+    func pagingViewController(_: PagingViewController, pagingItemAt index: Int) -> PagingItem {
+        <#code#>
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
