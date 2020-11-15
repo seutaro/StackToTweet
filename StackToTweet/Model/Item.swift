2ad13fc7ab9e4dc2e7a1923ebfa35bfcfb73f0f0
seutaro <seutaros@gmail.com>
2020-11-15 17:08:02 +0900

    Category.swift Item.swift modify

--- StackToTweet/Model/Item.swift
+++ StackToTweet/Model/Item.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import RealmSwift
+
+class Item: Object {
+    @objc dynamic var title: String = ""
+    @objc dynamic var done: Bool = false
+    
+    var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
+}
