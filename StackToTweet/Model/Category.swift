f0b2ebbe1c62298670b6d6ee81e77e73b90cbd88
seutaro <seutaros@gmail.com>
2020-11-15 16:57:24 +0900

    model file create

---
+++ StackToTweet/Model/Category.swift
@@ -0,0 +1,8 @@
+//
+//  Category.swift
+//  StackToTweet
+//
+//  Created by shotaro takahashi on 2020/11/15.
+//
+
+import Foundation
---
+++ StackToTweet/Model/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  StackToTweet
+//
+//  Created by shotaro takahashi on 2020/11/15.
+//
+
+import Foundation
