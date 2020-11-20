6e8688d621db699aed93d2b2b556c2b23781a649
seutaro <seutaros@gmail.com>
2020-11-21 01:08:53 +0900

    create addButton create protocol AddButtonDelegate - func addNewItem

---
+++ StackToTweet/Model/AddButtonDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AddTaskItem.swift
+//  StackToTweet
+//
+//  Created by shotaro takahashi on 2020/11/20.
+//
+
+import Foundation
+
+protocol AddTaskItem {
+    func addTaskItem(Item: String)
+}
