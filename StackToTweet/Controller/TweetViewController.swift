30781c5634252cb662c09dfd797bf7ff7b2d6e3a
seutaro <seutaros@gmail.com>
2020-12-16 22:48:16 +0900

    ツイッターへの画面遷移確認。以降はrealmからツイートに載せるcategoryとitemをソートする機能を記述する

--- StackToTweet/Controller/TweetViewController.swift
+++ StackToTweet/Controller/TweetViewController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
-class TweetViewController: UIViewController {
+class TweetViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+
+    
 
     @IBOutlet weak var tweetButton: UIButton!
     @IBOutlet weak var tweetTableView: UITableView!
@@ -17,19 +19,50 @@ class TweetViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tweetTableView.dataSource = self
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    
+    @IBAction func pressedTweetButton(_ sender: Any) {
+        let category = "swift"
+        let test = ["test","teすと"]
+        let text = getTweetText(from: category, items: test)
+        let encodedText = text.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        if let encodedText = encodedText,
+            let url = URL(string: "https://twitter.com/intent/tweet?text=\(encodedText)") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
     }
-    */
+    
+    func getTweetText(from category: String,items:[String]) -> String {
+        
+        var tweet = """
+                    #今日の積み上げ
+                    【#\(category)】
+                    """
+        
+        for item in items {
+            tweet = tweet + "\n\(item)"
+        }
+        
+        return tweet
+    }
+    
+    
+    //MARK: - tableviewDatasource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
 
 }
